289aea23c58323d3f604e902005252c232f58a5f
Toby Schrapel <toby@tobyschrapel.com>
2015-03-04 12:46:00 +0000

    Adding wpengine.sh, updating README

---
+++ README.md
@@ -0,0 +1,17 @@
+# WPEngine Roots / Bedrock build for WordPress
+WPEngine doesn't support things like Composer, Gulp, Bower, etc or allows the WordPress to be managed from a subdirectory. This script will go through the normal steps of the build process in a wpengine branch on the repository and make sure all the files needed are included.
+
+# Requirements
+* [Bedrock](https://github.com/roots/bedrock)
+* [Roots](https://github.com/roots/roots)
+
+# Usage
+1. Edit the name of the theme in `wpengine.sh`
+2. Ensure you use develop branch for staging, master for production locally
+3. Ensure you use staging, production remotes for WPEngine
+4. Run `sh wpengine.sh` followed by `staging` or `production`. E.g `sh wpengine.sh staging`
+
+# Todo
+* Remove output
+* Add helpful messages
+* Further error checking
---
+++ wpengine.sh
@@ -0,0 +1,59 @@
+environment=$1
+theme="sage"
+
+if [ "$environment" == "staging" ]
+then
+  git checkout develop
+elif [ "$environment" == "production" ]
+then
+  git checkout master
+else
+  echo "Invalid environment."
+  exit
+fi
+
+exists=`git show-ref refs/heads/wpengine`
+if [ -n "$exists" ]
+then
+  git branch -D wpengine
+fi
+git checkout -b wpengine
+
+cd "web/app/themes/${theme}"
+rm .gitignore
+npm install
+bower install
+if [ "$environment" == "staging" ]
+then
+  gulp
+elif [ "$environment" == "production" ]
+then
+  gulp --production
+fi
+cd ../../../../
+
+composer install
+
+mv web/app wp-content
+shopt -s extglob
+rm -rf !(wp-content)
+rm .DS_Store
+rm .env.example
+rm .gitignore
+rm .travis.yml
+rm wp-content/plugins/.gitkeep
+rm wp-content/themes/.gitkeep
+rm wp-content/uploads/.gitkeep
+
+git add .
+git commit -am "Setting up WPEngine build."
+if [ "$environment" == "staging" ]
+then
+  git push staging wpengine:master
+  git checkout develop
+elif [ "$environment" == "production" ]
+then
+  git push production wpengine:master
+  git checkout master
+fi
+git branch -D wpengine
