5719b2b15301fa036a0edca657feb2cbd57f3d84
Toby Schrapel <toby@tobyschrapel.com>
2015-03-04 15:45:13 +0000

    Assume composer, npm, bower are already setup. Rather than delete files, ignore them in .gitignore and remove them from cache.

--- wpengine.sh
+++ wpengine.sh
@@ -19,31 +19,12 @@ then
 fi
 git checkout -b wpengine
 
-cd "web/app/themes/${theme}"
+cp -r web/app wp-content
+rm "wp-content/themes/${theme}/.gitignore"
 rm .gitignore
-npm install
-bower install
-if [ "$environment" == "staging" ]
-then
-  gulp
-elif [ "$environment" == "production" ]
-then
-  gulp --production
-fi
-cd ../../../../
-
-composer install
-
-mv web/app wp-content
-shopt -s extglob
-rm -rf !(wp-content)
-rm .DS_Store
-rm .env.example
-rm .gitignore
-rm .travis.yml
-rm wp-content/plugins/.gitkeep
-rm wp-content/themes/.gitkeep
-rm wp-content/uploads/.gitkeep
+touch .gitignore
+echo "/*\n!wp-content/" >> .gitignore
+git ls-files | xargs git rm --cached
 
 git add .
 git commit -am "Setting up WPEngine build."
@@ -58,18 +39,3 @@ then
 fi
 git branch -D wpengine
 rm -rf wp-content/
-
-composer install
-
-cd "web/app/themes/${theme}"
-rm .gitignore
-npm install
-bower install
-if [ "$environment" == "staging" ]
-then
-  gulp
-elif [ "$environment" == "production" ]
-then
-  gulp --production
-fi
-cd ../../../../
